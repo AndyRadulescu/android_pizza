c3706d4c9d163bc07057516c4a91038680397986
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-11-20 17:08:37 +0200

    split the Form Functionality

---
+++ app/src/main/java/com/example/andy/vatradepizza/businessLogic/ContactInfoForm.java
@@ -0,0 +1,38 @@
+package com.example.andy.vatradepizza.businessLogic;
+
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactInfoForm {
+    protected List<EditText> formFields;
+    private static final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+    public ContactInfoForm(EditText name, EditText email, EditText phone) {
+        formFields = new ArrayList<>();
+        formFields.add(name);
+        formFields.add(email);
+        formFields.add(phone);
+    }
+
+    public boolean verifyFormIntegrity() {
+        boolean ok = true;
+        for (EditText field : formFields) {
+            if (field.getText().toString().equals("")) {
+                ok = false;
+                field.setHint("Obligatoriu de completat!");
+            }
+        }
+        return ok;
+    }
+
+    public boolean verifyPhoneLength() {
+        return formFields.get(2).getText().toString().length() >= 10;
+    }
+
+    public boolean verifyEmailFormat() {
+        return formFields.get(1).getText().toString().matches(emailPattern);
+    }
+}
--- app/src/main/java/com/example/andy/vatradepizza/businessLogic/RegisterFunctionality.java
+++ app/src/main/java/com/example/andy/vatradepizza/businessLogic/RegisterFunctionality.java
@@ -2,33 +2,24 @@ package com.example.andy.vatradepizza.businessLogic;
 
 import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class RegisterFunctionality {
-    private List<EditText> formFields;
-    private static final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
+public class RegisterFunctionality extends ContactInfoForm {
 
     public RegisterFunctionality(EditText name, EditText email, EditText phone, EditText password, EditText confirmPassword) {
-        formFields = new ArrayList<>();
-        formFields.add(name);
-        formFields.add(email);
-        formFields.add(phone);
+        super(name, email, phone);
         formFields.add(password);
         formFields.add(confirmPassword);
     }
 
-    public boolean verifyFormIntegrity() {
-        boolean ok = true;
-        for (EditText field : formFields) {
-            if (field.getText().toString().equals("")) {
-                ok = false;
-                field.setHint("Obligatoriu de completat!");
-            }
-        }
-        return ok;
-    }
+//    public boolean verifyFormIntegrity() {
+//        boolean ok = true;
+//        for (EditText field : formFields) {
+//            if (field.getText().toString().equals("")) {
+//                ok = false;
+//                field.setHint("Obligatoriu de completat!");
+//            }
+//        }
+//        return ok;
+//    }
 
     public boolean verifyPasswordSimilarity() {
         return formFields.get(3).getText().toString().equals(formFields.get(4).getText().toString());
@@ -36,13 +27,5 @@ public class RegisterFunctionality {
 
     public boolean verifyPasswordLength() {
         return formFields.get(3).getText().toString().length() >= 6;
-    }
-
-    public boolean verifyPhoneLength() {
-        return formFields.get(2).getText().toString().length() >= 10;
-    }
-
-    public boolean verifyEmailFormat() {
-        return formFields.get(1).getText().toString().matches(emailPattern);
     }
 }

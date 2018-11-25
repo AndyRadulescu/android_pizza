20224a2017ce96182235853931e8ded239f4ae67
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-11-25 16:06:14 +0200

    try to make the info from the scroll_pizza_menu get ot the menu_pizza_activity

--- app/src/main/java/com/example/andy/vatradepizza/menuFragments/PizzaFragment.java
+++ app/src/main/java/com/example/andy/vatradepizza/menuFragments/PizzaFragment.java
@@ -1,110 +1,54 @@
 package com.example.andy.vatradepizza.menuFragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.andy.vatradepizza.MenuPizzaActivity;
 import com.example.andy.vatradepizza.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PizzaFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PizzaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PizzaFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    View rootView;
+    LinearLayout listMenuItems;
 
     public PizzaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PizzaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PizzaFragment newInstance(String param1, String param2) {
-        PizzaFragment fragment = new PizzaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pizza, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        rootView = inflater.inflate(R.layout.fragment_pizza, container, false);
+        listMenuItems = rootView.findViewById(R.id.list_menu_items);
+        for (int i = 0; i < listMenuItems.getChildCount(); i++) {
+            LinearLayout item = (LinearLayout) listMenuItems.getChildAt(i);
+            int imageId = Integer.parseInt(String.valueOf(item.getChildAt(0).getTag()));
+            LinearLayout aux = (LinearLayout) item.getChildAt(1);
+            TextView pizzaName = (TextView) aux.getChildAt(0);
+            TextView pizzaDescription = (TextView) aux.getChildAt(1);
+            item.setOnClickListener(v -> {
+                // Perform action on click
+                Toast.makeText(getContext(), imageId + pizzaName.getText().toString() + pizzaDescription.getText().toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MenuPizzaActivity.class);
+                intent.putExtra("resId", imageId);
+                intent.putExtra("pizzaName", pizzaName.getText().toString());
+                intent.putExtra("pizzaDescription", pizzaDescription.getText().toString());
+                startActivity(intent);
+            });
         }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return rootView;
     }
 }

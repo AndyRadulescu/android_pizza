c7050faa6c1e54530f51f86b7bcc794683318eb8
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-11-25 16:05:35 +0200

    created menu pizza

--- app/src/main/java/com/example/andy/vatradepizza/MenuPizzaActivity.java
+++ app/src/main/java/com/example/andy/vatradepizza/MenuPizzaActivity.java
@@ -6,24 +6,37 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
 
 public class MenuPizzaActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView pizzaName, pizzaDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_pizza);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayOptions(R.drawable.ic_menu_manage);
 
+        imageView = findViewById(R.id.pizza_image);
+        pizzaName = findViewById(R.id.pizza_name);
+        pizzaDescription = findViewById(R.id.pizza_description);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            imageView.setImageResource(bundle.getInt("resId"));
+            pizzaName.setText(bundle.getInt("pizzaName"));
+            pizzaDescription.setText(bundle.getInt("pizzaDescription"));
+        }
+    }
 }

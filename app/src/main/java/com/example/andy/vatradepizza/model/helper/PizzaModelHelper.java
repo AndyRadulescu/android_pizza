17b67dde7d04131a29305c159e364f54874a68f1
AndyRadulescu <radulescu.eduard.andrei@gmail.com>
2018-11-27 19:17:47 +0200

    started the database service

--- app/src/main/java/com/example/andy/vatradepizza/model/helper/PizzaModelHelper.java
+++ app/src/main/java/com/example/andy/vatradepizza/model/helper/PizzaModelHelper.java
@@ -10,7 +10,7 @@ public class PizzaModelHelper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "pizza_table";
     private static final String ID = "id";
     private static final String PIZZA_NAME = "pizza_name";
-    private static final String PIZZA_DESCRIPRION = "pizza_description";
+    private static final String PIZZA_DESCRIPTION = "pizza_description";
     private static final String PIZZA_PRICE = "pizza_price";
     private static final String PIZZA_EXTRA_TOPPINGS = "pizza_extra_toppings";
     private static final String ORDER_TABLE_NAME = "order_table";
@@ -18,9 +18,9 @@ public class PizzaModelHelper extends SQLiteOpenHelper {
 
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + PizzaModelHelper.TABLE_NAME + " (" +
-                    PizzaModelHelper.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    PizzaModelHelper.ID + "UUID PRIMARY KEY," +
                     PizzaModelHelper.PIZZA_NAME + " TEXT," +
-                    PizzaModelHelper.PIZZA_DESCRIPRION + " TEXT," +
+                    PizzaModelHelper.PIZZA_DESCRIPTION + " TEXT," +
                     PizzaModelHelper.PIZZA_PRICE + " DOUBLE," +
                     PizzaModelHelper.PIZZA_EXTRA_TOPPINGS + " TEXT," +
                     "FOREIGN KEY(" + PizzaModelHelper.FOREIGN_ORDER_ID + ") REFERENCES " +
@@ -40,5 +40,9 @@ public class PizzaModelHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    private SQLiteDatabase getSQLDatabaseInstance() {
+        return this.getWritableDatabase();
     }
 }

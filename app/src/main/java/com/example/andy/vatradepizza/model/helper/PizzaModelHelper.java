93a4db2de6d05de2c864276750690b1e13377fd8
AndyRadulescu <radulescu.eduard.andrei@gmail.com>
2018-11-27 16:41:10 +0200

    created tables for pizza and souces

---
+++ app/src/main/java/com/example/andy/vatradepizza/model/helper/PizzaModelHelper.java
@@ -0,0 +1,41 @@
+package com.example.andy.vatradepizza.model.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PizzaModelHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "vatraDePizza.db";
+    private static final String TABLE_NAME = "souces";
+    private static final String ID = "id";
+    private static final String PIZZA_NAME = "pizza_name";
+    private static final String PIZZA_DESCRIPRION = "pizza_description";
+    private static final String PIZZA_PRICE = "pizza_price";
+    private static final String PIZZA_EXTRA_TOPPINGS = "pizza_extra_toppings";
+
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + PizzaModelHelper.TABLE_NAME + " (" +
+                    PizzaModelHelper.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    PizzaModelHelper.PIZZA_NAME + " TEXT," +
+                    PizzaModelHelper.PIZZA_DESCRIPRION + " TEXT," +
+                    PizzaModelHelper.PIZZA_PRICE + " DOUBLE," +
+                    PizzaModelHelper.PIZZA_EXTRA_TOPPINGS + " TEXT)";
+
+
+    PizzaModelHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}

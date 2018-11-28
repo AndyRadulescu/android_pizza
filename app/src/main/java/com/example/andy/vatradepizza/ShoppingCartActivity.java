1a21f433341ea6d0f6948d9ac2e8359fb2e2c4f4
AndyRadulescu <radulescu.eduard.andrei@gmail.com>
2018-11-28 19:11:51 +0200

    shopping cart activity shows database info

--- app/src/main/java/com/example/andy/vatradepizza/ShoppingCartActivity.java
+++ app/src/main/java/com/example/andy/vatradepizza/ShoppingCartActivity.java
@@ -1,12 +1,19 @@
 package com.example.andy.vatradepizza;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.example.andy.vatradepizza.database.helper.DatabaseHelper;
 
 import java.util.Objects;
 
 public class ShoppingCartActivity extends AppCompatActivity {
+
+    TextView dataBaseTestInfo;
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,25 @@ public class ShoppingCartActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayOptions(R.drawable.ic_menu_manage);
+
+        dataBaseTestInfo = findViewById(R.id.database_info);
+        dbHelper = new DatabaseHelper(this);
+        Cursor cursor = dbHelper.getAllPizzaData();
+        StringBuilder aux = new StringBuilder();
+        if (cursor.moveToFirst()) {
+            do {
+                aux.append("\n");
+                String id = cursor.getString(cursor.getColumnIndex("pizza_uuid"));
+                String availability = cursor.getString(cursor.getColumnIndex("pizza_name"));
+                String name = cursor.getString(cursor.getColumnIndex("pizza_description"));
+                Double price = cursor.getDouble(cursor.getColumnIndex("pizza_price"));
+                String extraToppings = cursor.getString(cursor.getColumnIndex("pizza_extra_toppings"));
+                aux.append(id).append(" ").append(availability).append(" ").append(name).append(" ").append(price).append(" ").append(extraToppings);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        dataBaseTestInfo.setText(aux.toString());
     }
 
     @Override

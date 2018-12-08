4d1ac9bf200c90cf2110cedc747cee1c435495db
AndyRadulescu <radulescu.eduard.andrei@gmail.com>
2018-12-08 22:28:58 +0200

    changed the database -> persistance

---
+++ app/src/main/java/com/example/andy/vatradepizza/persistance/model/PizzaModel.java
@@ -0,0 +1,98 @@
+package com.example.andy.vatradepizza.persistance.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class PizzaModel implements Serializable {
+    private String uuid;
+    private int imageId;
+    private String pizzaName;
+    private String pizzaDescription;
+    private double pizzaPrice;
+    private String toppings;
+    private ArrayList<SouceModel> souceList;
+
+    public PizzaModel() {
+    }
+
+    public PizzaModel(String uuid, int imageId, String pizzaName, String pizzaDescription, double pizzaPrice, String toppings, ArrayList<SouceModel> souceList) {
+        this.uuid = uuid;
+        this.imageId = imageId;
+        this.pizzaName = pizzaName;
+        this.pizzaDescription = pizzaDescription;
+        this.pizzaPrice = pizzaPrice;
+        this.toppings = toppings;
+        this.souceList = souceList;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getPizzaName() {
+        return pizzaName;
+    }
+
+    public void setPizzaName(String pizzaName) {
+        this.pizzaName = pizzaName;
+    }
+
+    public String getPizzaDescription() {
+        return pizzaDescription;
+    }
+
+    public void setPizzaDescription(String pizzaDescription) {
+        this.pizzaDescription = pizzaDescription;
+    }
+
+    public double getPizzaPrice() {
+        return pizzaPrice;
+    }
+
+    public void setPizzaPrice(double pizzaPrice) {
+        this.pizzaPrice = pizzaPrice;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public ArrayList<SouceModel> getSouceList() {
+        return souceList;
+    }
+
+    public void setSouceList(ArrayList<SouceModel> souceList) {
+        this.souceList = souceList;
+    }
+
+    @Override
+    public String toString() {
+        return "PizzaModel{" +
+                "uuid='" + uuid + '\'' +
+                ", imageId=" + imageId +
+                ", pizzaName='" + pizzaName + '\'' +
+                ", pizzaDescription='" + pizzaDescription + '\'' +
+                ", pizzaPrice=" + pizzaPrice +
+                ", toppings='" + toppings + '\'' +
+                ", souceList=" + souceList +
+                '}';
+    }
+}
+
+
---
+++ app/src/main/java/com/example/andy/vatradepizza/persistance/model/SouceModel.java
@@ -0,0 +1,53 @@
+package com.example.andy.vatradepizza.persistance.model;
+
+import java.io.Serializable;
+
+public class SouceModel implements Serializable {
+    private int id;
+    private String souceName;
+    private Integer souceQuantity;
+
+    public SouceModel(int id, String souceName, Integer souceQuantity) {
+        this.id = id;
+        this.souceName = souceName;
+        this.souceQuantity = souceQuantity;
+    }
+
+    public SouceModel() {
+
+    }
+
+    public SouceModel(String souceName, Integer souceQuantity) {
+        this.souceName = souceName;
+        this.souceQuantity = souceQuantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSouceName() {
+        return souceName;
+    }
+
+    public void setSouceName(String souceName) {
+        this.souceName = souceName;
+    }
+
+    public Integer getSouceQuantity() {
+        return souceQuantity;
+    }
+
+    public void setSouceQuantity(Integer souceQuantity) {
+        this.souceQuantity = souceQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return souceName + ' ' + souceQuantity;
+    }
+}
